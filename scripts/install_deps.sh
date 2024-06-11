7164ec0570a3a997581e9a8ac839440671655b54
Shenghang Tsai <jackalcooper@gmail.com>
2024-06-11 15:37:52 +0800

    Add repair (#2)

---
+++ scripts/install_deps.sh
@@ -0,0 +1,29 @@
+set -Eeuo pipefail
+which python
+which pip
+
+case "${TORCH_VERSION}" in
+  *dev*) use_nightly_torch=1 ;;
+  *    ) use_nightly_torch=0 ;;
+esac
+
+if [ ${use_nightly_torch} -eq 1 ]; then
+  pip install --pre -U \
+    setuptools_scm ninja Cython\
+    "torch==${TORCH_VERSION}" \
+    -r requirements.txt --extra-index-url https://download.pytorch.org/whl/nightly/cu${CUDA_SHORT_VERSION} --no-cache-dir
+else
+  pip install -U \
+    setuptools_scm ninja Cython\
+    "torch==${TORCH_VERSION}" \
+    -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu${CUDA_SHORT_VERSION} --no-cache-dir
+fi
+
+# Install NVIDIA cuDNN
+set -Eeuo pipefail
+has_cudnn=$(python -c "import importlib.util; assert importlib.util.find_spec('nvidia.cudnn') is not None" && echo 1 || echo 0)
+if [ ${has_cudnn} -eq 0 ]; then
+  cumajor=$(echo ${CUDA_SHORT_VERSION} | cut -c 1-2)
+  cudnn_required=$(python -c "import torch; cudnn_version = torch.backends.cudnn.version(); print(f'{cudnn_version // (10000 if cudnn_version >= 90000 else 1000)}.{(cudnn_version // 100) % 10}')")
+  python -c "import importlib.util; assert importlib.util.find_spec('nvidia.cudnn') is not None" || pip install "nvidia-cudnn-cu${cumajor}~=${cudnn_required}"
+fi
